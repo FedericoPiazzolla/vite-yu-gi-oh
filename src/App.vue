76a617ec7c20be8f3c659ebeddea0f96a911c0b7
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-27 17:36:48 +0100

    add: axios on AppVue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script >
+import axios from "axios";
+import { store } from "./store.js";
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+   data() {
+    return {
+      store
+    };
+  },
+  created() {
+    axios.get(this.store.apiUrl).then((resp) => {
+      this.store.characters = resp.data.result;
+    });
+  },
+  components: {
+    AppHeader,
+  }
+}
 
 </script>
 
 <template>
-
+<AppHeader/>
 </template>
 
 <style lang="scss">
